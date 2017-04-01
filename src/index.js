a9c91ddf7e099c840cc7c6d6f27c5cdde2b6d998
Sunday Nwuguru <sunday.nwuguru@andela.com>
2017-04-01 21:57:38 +0100

    Implement the store

--- src/index.js
+++ src/index.js
@@ -2,8 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { authentication } from './reducers/LoginReducer';
+
+
+function storeWrapper(state = {}, action) {
+    return {
+      auth: authentication(state.auth, action)
+    }
+}
+
+const store = createStore(
+    storeWrapper,
+    applyMiddleware(thunkMiddleware)
+);
+
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
