b54deaabd646cf1ddd268d17e461ef1feb299ce9
Sunday Nwuguru <sunday.nwuguru@andela.com>
2017-04-01 22:16:07 +0100

    Map auth state to auth modal props

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,11 +1,13 @@
 /**
  * Created by sundayguru on 01/04/2017.
  */
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import {Row, Col, Button, Modal, OverlayTrigger, Popover, Tooltip} from 'react-bootstrap';
 import SignupForm from './SignupForm';
 import LoginForm from './LoginForm';
+import { connect } from 'react-redux'
 import {isLogged} from '../Utils';
+import Loader from 'react-loader-advanced';
 
 
 class AuthModal extends Component {
@@ -31,54 +33,61 @@ class AuthModal extends Component {
 
 
   render() {
-      const popover = (
-          <Popover id="modal-popover" title="popover">
-            very popover. such engagement
-          </Popover>
-        );
-
-        const tooltip = (
-          <Tooltip id="modal-tooltip">
-            wow.
-          </Tooltip>
-        );
+      const {requesting} = this.props;
     return (
-        <div>
-            <p className="lead">Do you want to express yourself on the wall?</p>
-            <Row>
-                <Col xs={12} md={6}>
-                    <Button
-                      bsStyle="primary"
-                      bsSize="large"
-                      className="btn-block no-radius"
-                      onClick={this.openSignup}
-                    >
-                      Register
-                    </Button>
-                </Col>
-                <Col xs={12} md={6}>
-                    <Button
-                      bsStyle="primary"
-                      className="btn-block  no-radius"
-                      bsSize="large"
-                      onClick={this.openLogin}
-                    >
-                      Login
-                    </Button>
-                </Col>
-            </Row>
-            <hr/>
-            <Modal show={this.state.showModal} onHide={this.close}>
-              <Modal.Header closeButton>
-                <Modal.Title className="text-center text-uppercase">{this.state.title}</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                  {this.state.form}
-              </Modal.Body>
-            </Modal>
-          </div>
+        <Loader show={requesting} message={'Processing'}>
+            <div>
+                <p className="lead">Do you want to express yourself on the wall?</p>
+                <Row>
+                    <Col xs={12} md={6}>
+                        <Button
+                          bsStyle="primary"
+                          bsSize="large"
+                          className="btn-block no-radius"
+                          onClick={this.openSignup}
+                        >
+                          Register
+                        </Button>
+                    </Col>
+                    <Col xs={12} md={6}>
+                        <Button
+                          bsStyle="primary"
+                          className="btn-block  no-radius"
+                          bsSize="large"
+                          onClick={this.openLogin}
+                        >
+                          Login
+                        </Button>
+                    </Col>
+                </Row>
+                <hr/>
+                <Modal show={this.state.showModal} onHide={this.close}>
+                  <Modal.Header closeButton>
+                    <Modal.Title className="text-center text-uppercase">{this.state.title}</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                      {this.state.form}
+                  </Modal.Body>
+                </Modal>
+              </div>
+        </Loader>
     );
   }
 }
 
-export default AuthModal;
+
+AuthModal.propTypes = {
+  user: PropTypes.object.isRequired,
+  requesting: PropTypes.bool.isRequired,
+  dispatch: PropTypes.func.isRequired
+};
+
+
+function mapStateToProps(state, props) {
+    return {
+      user: state.user,
+      requesting: state.requesting
+    };
+}
+
+export default connect(mapStateToProps)(AuthModal);
