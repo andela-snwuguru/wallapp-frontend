76b763b9925b6e906a8212994fb8e83d0629990a
Sunday Nwuguru <sunday.nwuguru@andela.com>
2017-04-03 15:42:54 +0100

    Add wall actions for posting new message

---
+++ src/actions/WallActions.js
@@ -0,0 +1,22 @@
+/**
+ * Created by sundayguru on 03/04/2017.
+ */
+
+import {Config, post, eventAction} from '../Utils';
+
+export const POST_NEW_MESSAGE = "POST_NEW_MESSAGE";
+export const NEW_MESSAGE_POSTED = "NEW_MESSAGE_POSTED";
+export const NEW_MESSAGE_FAILED = "NEW_MESSAGE_FAILED";
+
+
+export function postNewMessage(data) {
+    return dispatch => {
+      dispatch(eventAction(POST_NEW_MESSAGE));
+      return post("walls/", data).then(payload => {
+            if (payload.id) {
+                return dispatch(eventAction(NEW_MESSAGE_POSTED));
+            }
+          dispatch(eventAction(NEW_MESSAGE_FAILED));
+      });
+    };
+}
